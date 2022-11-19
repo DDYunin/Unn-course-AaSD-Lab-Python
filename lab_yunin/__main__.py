323d8cf4cddaa3c6e670d55aa2112132a6a2969e
Dan Yunin <DaniilUnin2002@yandex.ru>
2022-11-19 13:51:42 +0300

    it is come back

--- lab_yunin/__main__.py
+++ lab_yunin/__main__.py
@@ -4,7 +4,7 @@
 
 from lab_yunin.my_graph import GraphLists, GraphListEdges
 from lab_yunin.my_disjoint_set import DisjointSetArray
-from lab_yunin.my_algorithms import kruskal_algorithm, prime_algorithm
+from lab_yunin.my_algorithms import kruskal_algorithm, prime_algorithm, prime_algorithm_2
 from lab_yunin.my_binary_heap import BinaryHeap
 from loguru import logger
 import time
@@ -20,7 +20,7 @@ def main():
     pass
 
 def kruskal_work_small_graph_1():
-    graph = GraphListEdges(9)
+    graph = GraphLists(9)
     graph.add_edge(0, 1, 4)
     graph.add_edge(0, 7, 8)
     graph.add_edge(1, 2, 8)
@@ -38,7 +38,7 @@ def kruskal_work_small_graph_1():
     kruskal_algorithm(graph, graph.vertexes)
 
 def kruskal_work_small_graph_2():
-    graph = GraphListEdges(6)
+    graph = GraphLists(6)
     graph.add_edge(0, 1, 7)
     graph.add_edge(0, 2, 8)
     graph.add_edge(1, 2, 11)
@@ -68,6 +68,24 @@ def prime_work_small_graph_1():
     graph.add_edge(7, 8, 7)
     prime_algorithm(graph, graph.vertexes)
 
+def prime_work_small_graph_1_2():
+    graph = GraphListEdges(9)
+    graph.add_edge(0, 1, 4)
+    graph.add_edge(0, 7, 8)
+    graph.add_edge(1, 2, 8)
+    graph.add_edge(1, 7, 11)
+    graph.add_edge(2, 3, 7)
+    graph.add_edge(2, 8, 2)
+    graph.add_edge(2, 5, 4)
+    graph.add_edge(3, 4, 9)
+    graph.add_edge(3, 5, 14)
+    graph.add_edge(4, 5, 10)
+    graph.add_edge(5, 6, 2)
+    graph.add_edge(6, 7, 1)
+    graph.add_edge(6, 8, 6)
+    graph.add_edge(7, 8, 7)
+    prime_algorithm_2(graph, graph.vertexes)
+
 def prime_work_small_graph_2():
     graph = GraphLists(6)
     graph.add_edge(0, 1, 7)
@@ -84,25 +102,40 @@ def prime_work_small_graph_2():
 def prime_work_large_graph():
     test_graph = GraphLists(10000)
     start_time = time.monotonic()
-    test_graph.create_edges_naive(5000*9999)
+    test_graph.create_edges_naive(5000*500)
     print('The creating edges lasted {0} seconds'.format(time.monotonic() - start_time))
     start_time = time.monotonic()
     prime_algorithm(test_graph, test_graph.vertexes)
     print('The prime algorithm edges lasted {0} seconds'.format(time.monotonic() - start_time))
 
 def kruskal_work_large_graph():
-    test_graph = GraphListEdges(10000)
+    test_graph = GraphLists(1000)
     start_time = time.monotonic()
-    test_graph.create_edges_naive(5000*9999)
+    test_graph.create_edges_naive(500*500)
     print('The creating edges lasted {0} seconds'.format(time.monotonic() - start_time))
     start_time = time.monotonic()
     kruskal_algorithm(test_graph, test_graph.vertexes)
     print('The kruskal algorithm edges lasted {0} seconds'.format(time.monotonic() - start_time))
 
+# EXPERIMENTS
+# n = 10000, m = 5000*9999
+# n = 10000, m = 5000*5
+# n = 10000, m = 5000*500
+
+# n = 1000, m = 500*999
+# n = 1000, m = 500*5
+# n = 1000, m = 500*500
+
+# n = 100, m = 50*99
+# n = 100, m = 50*5
+# n = 100, m = 50*50
+
+
 if __name__ == '__main__':
     # kruskal_work_small_graph_1()
+    prime_work_small_graph_1()
+    prime_work_small_graph_1_2()
     # kruskal_work_small_graph_2()
-    # prime_work_small_graph_1()
     # prime_work_small_graph_2()
     # kruskal_work_large_graph()
-    prime_work_large_graph()
+    # prime_work_large_graph()
--- lab_yunin/my_algorithms.py
+++ lab_yunin/my_algorithms.py
@@ -2,7 +2,7 @@ from lab_yunin.my_disjoint_set import DisjointSetArray
 from lab_yunin.my_binary_heap import BinaryHeap
 
 
-def kruskal_algorithm(graph, vertexes):
+def kruskal_algorithm_2(graph, vertexes):
     current_number_edge, number_edges_graph = 0, 0
     my_set = DisjointSetArray(vertexes)
     graph.graph = sorted(graph.graph, key=lambda item: item[2])
@@ -25,6 +25,36 @@ def kruskal_algorithm(graph, vertexes):
     # print('\nShort information:\n')
     # print('Number edges: {0}\nWeight: {1}'.format(len(min_link_tree), weight_subgraph))
     # print('\n\tInforamtion about MST\n')
+
+def kruskal_algorithm(graph, vertexes):
+    current_number_edge, number_edges_graph = 0, 0
+    my_set = DisjointSetArray(vertexes)
+    list_edges = []
+    print(graph.graph)
+    for vertex in range(vertexes):
+        for edge in graph.graph[vertex]:
+            list_edges.append((vertex, edge[0], edge[1]))
+    list_edges = sorted(list_edges, key=lambda item: item[2])
+    min_link_tree = []
+    while number_edges_graph < vertexes - 1:
+        first_vertex, second_vertex, weight = list_edges[current_number_edge]
+        current_number_edge += 1
+        value_set_first_vertex = my_set.find(first_vertex)
+        value_set_second_vertex = my_set.find(second_vertex)
+        if value_set_first_vertex != value_set_second_vertex:
+            number_edges_graph += 1
+            min_link_tree.append([first_vertex,second_vertex,weight])
+            my_set.union(value_set_first_vertex,value_set_second_vertex)
+    # print('\n\tInforamtion about MST\n')
+    # print('Struct of subgraph:\n')
+    # weight_subgraph = 0
+    # for edge in min_link_tree:
+    #     print('Edge: ({0},{1}) , Weight: {2}'.format(edge[0], edge[1], edge[2]))
+    #     weight_subgraph += edge[2]
+    # print('\nShort information:\n')
+    # print('Number edges: {0}\nWeight: {1}'.format(len(min_link_tree), weight_subgraph))
+    # print('\n\tInforamtion about MST\n')
+
 
 # TODO Мне кажется, что что-то тут не так
 def prime_algorithm(graph, vertexes):
@@ -52,15 +82,56 @@ def prime_algorithm(graph, vertexes):
                 key[second_vertex] = current_edge[1]
                 min_link_tree[second_vertex] = [current_vertex, current_edge[1]]
                 min_heap.decrease_key(second_vertex, key[second_vertex])
-    # print('\n\tInforamtion about MST\n')
-    # print('Struct of subgraph:\n')
-    # weight_subgraph = 0
-    # for vertex in range(vertexes):
-    #     if not min_link_tree[vertex]:
-    #         print('пропускаю')
-    #         continue
-    #     print('Edge: ({0},{1}) , Weight: {2}'.format(vertex, min_link_tree[vertex][0], min_link_tree[vertex][1]))
-    #     weight_subgraph += min_link_tree[vertex][1]
-    # print('\nShort information:\n')
-    # print('Number edges: {0}\nWeight: {1}'.format(len(min_link_tree) - 1, weight_subgraph))
-    # print('\n\tInforamtion about MST\n')
+    print('\n\tInforamtion about MST\n')
+    print('Struct of subgraph:\n')
+    weight_subgraph = 0
+    for vertex in range(vertexes):
+        if not min_link_tree[vertex]:
+            print('пропускаю')
+            continue
+        print('Edge: ({0},{1}) , Weight: {2}'.format(vertex, min_link_tree[vertex][0], min_link_tree[vertex][1]))
+        weight_subgraph += min_link_tree[vertex][1]
+    print('\nShort information:\n')
+    print('Number edges: {0}\nWeight: {1}'.format(len(min_link_tree) - 1, weight_subgraph))
+    print('\n\tInforamtion about MST\n')
+
+def prime_algorithm_2(graph, vertexes):
+    # Суть алгоритма в том, чтобы на каждом шаге искать ребро с минимальным весом и присоединять его к дереву
+    # Причём поиск этого минимального ребра должен быть на двоичной куче
+    min_link_tree = {} # mst
+    key = []
+    min_heap = BinaryHeap()
+    # ЗАПОЛНЕНИЕ КУЧИ
+    for vertex in range(vertexes):
+        min_link_tree.setdefault(vertex, [])
+        key.append(float('inf'))
+        min_heap.heap_list.append([vertex, key[vertex]])
+        min_heap.position.append(vertex)
+    min_heap.position[0] = 0
+    key[0] = 0
+    min_heap.decrease_key(0, key[0])
+    min_heap.heap_size = vertexes
+    print(graph.graph)
+    while min_heap.heap_size != 0:
+        node = min_heap.get_min_node()
+        current_vertex = node[0]
+        for current_edge in graph.graph:
+            if current_edge[0] == current_vertex:
+                second_vertex = current_edge[1]
+                if min_heap.is_in_min_heap(second_vertex) and current_edge[2] < key[second_vertex]:
+                    key[second_vertex] = current_edge[2]
+                    min_link_tree[second_vertex] = [current_vertex, current_edge[2]]
+                    min_heap.decrease_key(second_vertex, key[second_vertex])
+    print(min_link_tree)
+    print('\n\tInforamtion about MST\n')
+    print('Struct of subgraph:\n')
+    weight_subgraph = 0
+    for vertex in range(vertexes):
+        if not min_link_tree[vertex]:
+            print('пропускаю')
+            continue
+        print('Edge: ({0},{1}) , Weight: {2}'.format(vertex, min_link_tree[vertex][0], min_link_tree[vertex][1]))
+        weight_subgraph += min_link_tree[vertex][1]
+    print('\nShort information:\n')
+    print('Number edges: {0}\nWeight: {1}'.format(len(min_link_tree) - 1, weight_subgraph))
+    print('\n\tInforamtion about MST\n')
